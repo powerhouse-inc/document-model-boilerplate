3672b8704c5097c804839555468c211789663f19
Guillermo Puente <guillermo.ps09@gmail.com>
2024-11-25 16:34:43 -0300

    added storybook as optional

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,37 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import { getConfig } from '@powerhousedao/codegen';
-
-const { editorsDir } = getConfig();
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-    stories: [`../${editorsDir}/**/*.stories.@(js|jsx|mjs|ts|tsx)`],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        './addons/operations-preset.ts',
-    ],
-    framework: {
-        name: '@storybook/react-vite',
-        options: {},
-    },
-    docs: {
-        autodocs: 'tag',
-    },
+  stories: [`../editors/**/*.stories.tsx`],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "./addons/operations-preset.ts",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  typescript: {
+    reactDocgen: false,
+  },
+  core: {
+    builder: {
+      name: "@storybook/builder-vite",
+      options: {
+        viteConfigPath: "vite.config.ts",
+        rollupOptions: {
+          external: [
+            /@codemirror\/.*/,
+            'thememirror',
+            'cm6-graphql'
+          ]
+        }
+      }
+    }
+  }
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,19 @@
-import type { Preview } from '@storybook/react';
+import type { Preview } from "@storybook/react";
+import { mockDateDecorator } from "storybook-mock-date-decorator";
+import "../editors/global.css";
 
 const preview: Preview = {
-    parameters: {
-        actions: { argTypesRegex: '^on[A-Z].*' },
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/,
-            },
-        },
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
+    date: new Date("March 10, 2021 10:00:00"),
+  },
 };
+
+export const decorators = [mockDateDecorator];
 
 export default preview;
